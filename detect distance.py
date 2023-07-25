f3f2c5ace035cbabf85514e9c4a49eb3edeaabb5
raavisingh-002 <106227846+raavisingh-002@users.noreply.github.com>
2023-07-25 20:23:14 +0530

    Add files via upload

---
+++ detect distance.py
@@ -0,0 +1,29 @@
+import cv2
+import pyrealsense2
+from realsense_depth import *
+
+selected_point = (400, 300)
+
+def update_selected_point(event, x, y, flags, param):
+    global selected_point
+    if event == cv2.EVENT_LBUTTONDOWN:
+        selected_point = (x, y)
+
+depth_camera = DepthCamera()
+
+cv2.namedWindow("Color frame")
+cv2.setMouseCallback("Color frame", update_selected_point)
+
+while True:
+    ret, depth_frame, color_frame = depth_camera.get_frame()
+
+    cv2.circle(color_frame, selected_point, 4, (0, 0, 255))
+    distance_mm = depth_frame[selected_point[1], selected_point[0]]
+    cv2.putText(color_frame, "{}mm".format(distance_mm), (selected_point[0], selected_point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+    cv2.imshow("Depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break    
